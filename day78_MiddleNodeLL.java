366651aee4274a7eef81c0949d072a9b0698cafc
Saravana Priyan <c.saravanapriyan@gmail.com>
2025-12-30 20:55:51 +0530

    day 79

--- day78_MiddleNodeLL.java
+++ day78_MiddleNodeLL.java
@@ -13,6 +13,19 @@ class ListNode {
 
 public class day78_MiddleNodeLL {
 
+    public static ListNode insertAtEnd(ListNode head, int data) {
+    if (head == null) {
+        return new ListNode(data);
+    }
+
+    ListNode temp = head;
+    while (temp.next != null) {
+        temp = temp.next;
+    }
+    temp.next = new ListNode(data);
+    return head;
+}
+
     // Method to find middle node
     public static ListNode MiddleNode(ListNode head) {
         ListNode temp = head;
@@ -55,3 +68,44 @@ public class day78_MiddleNodeLL {
         sc.close();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* fast and slow pointer
+
+two pointers ptr1 & ptr2
+
+fast moves twice as FAST as slow 
+slow moves at one step at time 
+
+by the time fast has reached the end 
+slow is at the middle 
+ */
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+}
+
+
+
---
+++ day79.java
@@ -0,0 +1,83 @@
+//HAS CYCLE ---> FLOYDS CYCLE ---> SLOW AND FAST POINTER
+
+import java.util.*;
+
+class ListNode {
+    int data;
+    ListNode next, head;
+
+    ListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class day79 {
+    public static Boolean Brute_LLCycled(ListNode head) {
+        ListNode temp = head;
+        Set<ListNode> set = new HashSet<>();
+        
+        while(temp != null) {
+            if(!set.add(temp)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+
+        return false;
+    }
+
+
+    public static Boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        if(slow == fast) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ListNode head = new ListNode(sc.nextInt());
+        ListNode temp = head;
+
+        for (int i = 1; i < n; i++) {
+            temp.next = new ListNode(sc.nextInt());
+            temp = temp.next;
+        }
+
+        temp.next = head;
+
+        System.out.println(Brute_LLCycled(head));
+
+        sc.close();
+    }
+}
+
+}
